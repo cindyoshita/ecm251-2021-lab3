bd93d1ed7e09d3b6960e37b09197edf7e363b380
alice270 <77018227+alice270@users.noreply.github.com>
2021-02-25 09:29:53 -0300

    mudancasAula01

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,7 @@
+class OlaMundo{
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+        System.out.println("Ola!");
+        System.out.println("Ola Ola");
+    }
+}
