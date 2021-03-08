1d54a41f71d03d86426eb88fdcb0cb0df03f61c3
alice270 <77018227+alice270@users.noreply.github.com>
2021-03-08 13:09:53 -0300

    criação de dados de um banco(aula03)

---
+++ aula03/src/yoshita/cindy/Conta.java
@@ -0,0 +1,47 @@
+package yoshita.cindy;
+
+import java.sql.SQLOutput;
+
+public class Conta {
+    int numero;
+    double saldo;
+    Cliente cliente;
+
+
+    void visualizarSaldo() {
+        double saldo = this.saldo; // so mexi na variável local (199.99) se eu printar 2 vezes ele volta no atributo inicial do this(199.99)
+        //se eu tirar esse double se printar 2 vezes na segunda vez volta a fazer o processo com o q restou
+        System.out.println("Saldo da conta : R$" + this.saldo);
+        /*while(saldo > 0){
+            System.out.println("Gastando!!!" + saldo);
+            saldo = saldo - 10;
+        }*/
+    }
+    void depositar(double valor) {
+        //this.saldo = this.saldo +valor;
+        this.saldo += valor;
+    }
+    boolean sacar(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    boolean transferirPara(double valor, Conta destino) {
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "numero=" + numero +
+                ", saldo=" + saldo +
+                ", cliente=" + cliente.toString() +
+                '}';
+    }
+}
