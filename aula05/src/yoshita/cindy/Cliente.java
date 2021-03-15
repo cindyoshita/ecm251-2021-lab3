bfdef2f53e25b8721dee862fa5b225d0aa41a489
alice270 <77018227+alice270@users.noreply.github.com>
2021-03-15 13:13:49 -0300

    aula 05 alterações

---
+++ aula05/src/yoshita/cindy/Cliente.java
@@ -0,0 +1,18 @@
+package yoshita.cindy;
+
+public class Cliente {
+    private String titular;
+    private String cpf;
+
+    public Cliente(String titular) {
+        this.titular = titular;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "titular='" + titular + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
