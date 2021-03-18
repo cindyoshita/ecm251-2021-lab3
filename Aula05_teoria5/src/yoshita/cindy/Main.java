064d7c0f33216c37b00f25a174c4d1f06d9c4961
alice270 <77018227+alice270@users.noreply.github.com>
2021-03-18 09:29:17 -0300

    aula05-teoria

---
+++ Aula05_teoria5/src/yoshita/cindy/Main.java
@@ -0,0 +1,10 @@
+package yoshita.cindy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "529.982.247-25";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+
+    }
+}
